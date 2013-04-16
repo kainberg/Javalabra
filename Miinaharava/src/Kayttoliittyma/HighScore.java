fc06197a9ba29be9014b9645a21e81481e37a890
Alexander Kainberg <alexander.kainberg@helsinki.fi>
2013-04-16 19:34:56 +0300

    5.viikon asiat. Bugin korjaus ja highscore puuttuu.

---
+++ Miinaharava/src/Kayttoliittyma/HighScore.java
@@ -0,0 +1,20 @@
+package Kayttoliittyma;
+
+import java.io.*;
+
+/**
+ * Luokka joka pitää kirjaa highscoresta.
+ *
+ * @author kainberg
+ */
+public class HighScore {
+
+    public String palautaHighScore() {
+        //metodi palauttaa highscoren
+        return null;
+    }
+
+    public void updateHighScore(int uusiHighScore) {
+        //metodi muuttaa highscoren
+    }
+}
